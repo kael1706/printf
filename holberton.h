b57c0b3191498d22e074d3c17f9f77fa73d6f6ef
kael <cdcp-1795@hotmail.com>
2019-10-28 22:56:49 +0000

    add holberton.h

---
+++ holberton.h
@@ -0,0 +1,23 @@
+#ifndef _FL_HOLBERTON
+#define _FL_HOLBERTON
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+* struct manager - function manager
+* Description: obj of type manager
+* @cs: conversion specifier
+* @f: function related
+*/
+typedef struct manager
+{
+	char cs;
+	int (*f)(va_list);
+} managerF;
+int _printf(const char *format, ...);
+int (*f_controller(char cs))(va_list);
+int _putchar(char c);
+int f_char(va_list vl);
+int fe_1(va_list vl);
+#endif
